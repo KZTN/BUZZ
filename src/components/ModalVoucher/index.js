3dd49d9e067dfd31b3e01d103bd82280bcacd22c
KZTN <kakaiocesar2@gmail.com>
2020-05-16 16:54:10 -0300

    :bug: Fixzed many erros on ModalVoucher component and card Component
    all changes are functional

--- src/components/ModalVoucher/index.js
+++ src/components/ModalVoucher/index.js
@@ -1,30 +1,35 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './styles.scss';
 import { FaTimes, FaWhatsapp } from 'react-icons/fa';
 import { FiStar } from 'react-icons/fi';
 
 export default function ModalVoucher({ voucher, onClick }) {
+  function popupWPP() {
+    window.open(
+            `https://wa.me/55${voucher.seller.phone}`,
+            '_top'
+        );
+}
   async function handleCloseModal() {
-    await onClick({
-      });
+    await onClick({});
   }
-      useEffect(() => {
-        const listener = (e) => {
-            if (e.key === 'Escape') {
-              handleCloseModal();
-            }
-        };
-        window.addEventListener('keydown', listener);
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === 'Escape') {
+        handleCloseModal();
+      }
+    };
+    window.addEventListener('keydown', listener);
 
-        return () => {
-            window.removeEventListener('keydown', listener);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener('keydown', listener);
+    };
+  }, []);
   return (
-    <div className="modal" >
+    <div className="modal">
       <div className="content">
         <div className="close-button">
-  <span>#{voucher._id}</span>
+          <span>#{voucher._id}</span>
           <button onClick={handleCloseModal}>
             <FaTimes size={24} color="#444" />
           </button>
@@ -34,11 +39,11 @@ export default function ModalVoucher({ voucher, onClick }) {
             <img src={voucher.photo} alt="" srcset="" />
           </div>
           <div className="box-right">
-            <h1>{voucher.name}</h1>
-            <h3>{voucher.seller.name}</h3>
-            <h4>Valor: R${voucher.price}</h4>
-            <h4>Quantidade: {voucher.amount}</h4>
-            <div className="contact">
+            <h1><strong>{voucher.name}</strong></h1>
+            <span>{voucher.seller.name}</span>
+            <h1>R${voucher.price}</h1>
+            <span>Quantidade: {voucher.amount}</span>
+            <div className="contact" onClick={popupWPP}>
               <FaWhatsapp size={24} color="#fff" style={{ marginRight: 5 }} />
               <span>Entre em contato</span>
             </div>
