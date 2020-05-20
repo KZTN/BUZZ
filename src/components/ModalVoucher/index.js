0d5c7a02fb325c1415da48fbef84904f6456e78f
KZTN <kakaiocesar2@gmail.com>
2020-05-20 15:34:57 -0300

    :zap: Improved ModalVoucher performance
    all changes are functional

--- src/components/ModalVoucher/index.js
+++ src/components/ModalVoucher/index.js
@@ -1,15 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.scss';
 import { FaTimes, FaWhatsapp } from 'react-icons/fa';
 import { FiStar } from 'react-icons/fi';
+import api from '../../services/api';
+import { useCookies } from 'react-cookie';
 
-export default function ModalVoucher({ voucher, onClick }) {
+export default function ModalVoucher({ user, voucher, onClick }) {
+  const [cookies, setCookie, removeCookie] = useCookies(['token']);
+
+  const { isfavorite, setIsfavorite } = useState(false);
+
+  async function handleFavorite() {
+    await api
+      .put(`/favorites/${voucher._id}`, {
+        headers: { Authorization: `Bearer ${cookies.token}` },
+      })
+      .then(() => {
+        if (isfavorite) {
+          setIsfavorite(false);
+        } else {
+          setIsfavorite(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  user.favorites.map((favitem) => {
+    if (favitem == voucher._id) {
+      setIsfavorite(true);
+    }
+  });
   function popupWPP() {
-    window.open(
-            `https://wa.me/55${voucher.seller.phone}`,
-            '_top'
-        );
-}
+    window.open(`https://wa.me/55${voucher.seller.phone}`, '_top');
+  }
   async function handleCloseModal() {
     await onClick({});
   }
@@ -39,17 +63,29 @@ export default function ModalVoucher({ voucher, onClick }) {
             <img src={voucher.photo} alt="" srcset="" />
           </div>
           <div className="box-right">
-            <h1><strong>{voucher.name}</strong></h1>
+            <h1>
+              <strong>{voucher.name}</strong>
+            </h1>
             <span>{voucher.seller.name}</span>
             <h1>R${voucher.price}</h1>
-            <span>Quantidade: {voucher.amount}</span>
+            {voucher.amount > 1 ? (
+              <span>Quantidade: {voucher.amount}</span>
+            ) : voucher.amount === 1 ? (
+              <span>Último disponível</span>
+            ) : voucher.amount === 0 ? (
+              <span>Esgotado</span>
+            ) : null}
             <div className="contact" onClick={popupWPP}>
               <FaWhatsapp size={24} color="#fff" style={{ marginRight: 5 }} />
               <span>Entre em contato</span>
             </div>
-            <div className="favorites">
-              <FiStar size={24} color="#666" />{' '}
-              <span>Adicionar a sua lista de desejos</span>
+            <div className="favorites" onClick={handleFavorite}>
+              <FiStar size={24} color="#666" />
+              {!isfavorite ? (
+                <span>Adicionar a sua lista de desejos</span>
+              ) : (
+                <span>Remover da sua lista de desejos</span>
+              )}
             </div>
           </div>
         </div>
