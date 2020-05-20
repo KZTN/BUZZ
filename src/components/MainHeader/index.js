8b14986be4945c0e156536b48b2a4be411371b4c
KZTN <kakaiocesar2@gmail.com>
2020-05-20 15:34:07 -0300

    :boom: Changes Herder to MainHeader filename
    all changes are functional

---
+++ src/components/MainHeader/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles.scss';
+
+export default function Header() {
+  return (
+    <header id="mainheader">
+        <div className="box-logo">
+          <h1>buz</h1>
+          <h1>z</h1>
+        </div>
+        <div className="box-text">
+          <span>Seu aplicativo de vouchers</span>
+        </div>
+      <div className="box-actions">
+          <div className="box-account">
+          <Link to="/BUZZ/profile"><span>Minha conta</span></Link>
+          </div>
+          <div className="box-voucher">
+             <Link to="/BUZZ/vouchers"><span>Vouchers</span></Link>
+          </div>
+          <div className="box-sell">
+             <span>Venda</span>
+          </div>
+          <div className="box-about">
+             <span>Sobre</span>
+          </div>
+      </div>
+    </header>
+  );
+}
